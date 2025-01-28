07f470099f0b7d871cd3a44ca9fe43dd5f276b14
sasaSilver <al.mikhailov@innopolis.university>
2025-01-28 17:36:59 +0300

    added table indexes for username and room & date

--- bot/database/schemas/booking_schema.py
+++ bot/database/schemas/booking_schema.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, Date, Time, Integer
+from sqlalchemy import String, Date, Time, Integer, Index, CheckConstraint, func
 from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
 
 class Base(DeclarativeBase):
@@ -8,9 +8,16 @@ class BookingSchema(Base):
     __tablename__ = "bookings"
     
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    username: Mapped[str] = mapped_column(String)
-    user_full_name: Mapped[str] = mapped_column(String)
-    room: Mapped[str] = mapped_column(String)
-    date: Mapped[Date] = mapped_column(Date)
-    start_time: Mapped[Time] = mapped_column(Time)
-    end_time: Mapped[Time] = mapped_column(Time) 
+    username: Mapped[str] = mapped_column(String, nullable=False)
+    user_full_name: Mapped[str] = mapped_column(String, nullable=False)
+    room: Mapped[str] = mapped_column(String, nullable=False)
+    date: Mapped[Date] = mapped_column(Date, nullable=False)
+    start_time: Mapped[Time] = mapped_column(Time, nullable=False)
+    end_time: Mapped[Time] = mapped_column(Time, nullable=False)
+    
+    __table_args__ = (
+        Index('idx_username', 'username'),
+        Index('idx_room_date', 'room', 'date'),
+        CheckConstraint('end_time > start_time', name='check_time_order'),
+        CheckConstraint(date >= func.current_date(), name='check_future_date'),
+    )
